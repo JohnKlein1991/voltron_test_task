056d8e4b02ff84c05147a745e3dcaf408302a124
vagrant <vagrant@homestead>
2020-02-09 13:09:28 +0000

    initial commit

---
+++ common/config/params.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'krentoamoranto@yandex.ru',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+    'user.passwordResetTokenExpire' => 3600,
+];
