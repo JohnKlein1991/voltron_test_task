dc769e646950c517b939baf9370df91417a53a52
andrew_marhai <buk2018irinam@gmail.com>
2020-02-17 08:03:12 +0300

    Сервис и репозиторий для статистики

---
+++ frontend/services/StatisticService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace frontend\services;
+
+use frontend\models\MoneyTransaction;
+use frontend\services\repositories\StatisticRepository;
+
+/**
+ * Class StatisticService
+ * @package frontend\services
+ */
+class StatisticService
+{
+    /**
+     * @var StatisticRepository
+     */
+    private $repository;
+
+    /**
+     * StatisticService constructor.
+     * @param StatisticRepository $repository
+     */
+    public function __construct(StatisticRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     * @throws \yii\db\Exception
+     */
+    public function getDataForStatisticTable($data = [])
+    {
+        // значения по умолчанию, при переходе на страницу
+        $dateFrom = $data['date_from'] ?? date('Y-m-d');
+        $dateTo = $data['date_to'] ?? date('Y-m-d');
+        $grouping = $data['group_by'] ?? 'day';
+        $categoryId = $data['category_id'] ?? 0;
+        $companyId = $data['company_id'] ?? 0;
+
+        $rawData = $this->repository->getDataForStatistic($dateFrom, $dateTo, $companyId, $categoryId, $grouping);
+        return $this->prepareRawDataForStatistic($rawData);
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    private function prepareRawDataForStatistic($data)
+    {
+
+        foreach ($data as $key => $item) {
+            if ($item['type'] == MoneyTransaction::$TYPE_REVENUE) {
+                $data[$key]['type'] = 'Доход';
+            } else if ($item['type'] == MoneyTransaction::$TYPE_EXPENSE) {
+                $data[$key]['type'] = 'Расход';
+            }
+        }
+        return $data;
+    }
+}
---
+++ frontend/services/repositories/StatisticRepository.php
@@ -0,0 +1,77 @@
+<?php
+
+
+namespace frontend\services\repositories;
+
+
+use frontend\models\StatisticForm;
+use Yii;
+
+/**
+ * Class StatisticRepository
+ * @package frontend\services\repositories
+ */
+class StatisticRepository
+{
+    /**
+     * @var \yii\db\Connection
+     */
+    private $connection;
+
+    /**
+     * StatisticRepository constructor.
+     */
+    public function __construct()
+    {
+        $this->connection = Yii::$app->getDb();
+    }
+
+    /**
+     * @param $dateFrom
+     * @param $dateTo
+     * @param $companyId
+     * @param $categoryId
+     * @param $grouping
+     * @return array
+     * @throws \yii\db\Exception
+     */
+    public function getDataForStatistic($dateFrom, $dateTo, $companyId, $categoryId, $grouping)
+    {
+        $rawSqlQuery = $this->getRawSqlQuery($dateFrom, $dateTo, $companyId, $categoryId, $grouping);
+        return $this->connection->createCommand($rawSqlQuery)->queryAll();
+    }
+
+    /**
+     * @param $dateFrom
+     * @param $dateTo
+     * @param $companyId
+     * @param $categoryId
+     * @param $grouping
+     * @return string
+     */
+    private function getRawSqlQuery($dateFrom, $dateTo, $companyId, $categoryId, $grouping)
+    {
+        $companySelect = ($companyId == 0) ? "c.title as company," : "";
+        $categorySelect = ($categoryId == 0) ? "mtc.title as category," : "";
+        $companyWhere = $companyId ? "and c.id = ${companyId}" : "";
+        $categoryWhere = $categoryId ? "and mtc.id = ${categoryId}" : "";
+        $companyGroupBy = ($companyId == 0) ? "company," : "";
+        $categoryGroupBy = ($categoryId == 0) ? "category," : "";
+        if ($grouping === StatisticForm::$GROUP_BY_MONTHS_VALUE) {
+            $dateSelect = "concat(year(mt.date),'-',month(mt.date)) as date,";
+        } else {
+            $dateSelect = "mt.date as date,";
+        }
+        $dateGroupBy = "date,";
+
+        return "
+            select {$dateSelect} {$companySelect} {$categorySelect} mt.type, sum(amount) as amount
+            from money_transactions mt
+            join companies c on mt.company_id = c.id
+            join money_transactions_categories mtc on mt.category_id = mtc.id
+            where mt.date between '{$dateFrom}' and '{$dateTo}'
+            {$companyWhere} {$categoryWhere}
+            group by {$dateGroupBy} {$companyGroupBy} {$categoryGroupBy} mt.type;
+        ";
+    }
+}
